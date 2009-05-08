e24ef5009adb28ced355cd02b26a7e83d491f13b
Jeff Hodges <jeff@somethingsimilar.com>
2009-05-08 02:17:22 -0700

    removing some trailing whitespace

--- test/test_minhash.rb
+++ test/test_minhash.rb
@@ -6,10 +6,10 @@ class TestMinHash < Test::Unit::TestCase
   def test_minhash_works
     m = MinHash.minhash(["foo", "bar", "baz"], 23)
     assert_equal 1157430835185436796, m
-    
+
     m = MinHash.minhash(["bar", "foo", "baz"], 23)
     assert_equal 1157430835185436796, m
-    
+
     m = MinHash.minhash(["baz", "bar", "foo"], 23)
     assert_equal 1157430835185436796, m
   end
@@ -17,10 +17,10 @@ class TestMinHash < Test::Unit::TestCase
   def test_minhash32_works
     m = MinHash.minhash32(["foo", "bar", "baz"], 23)
     assert_equal 568256043, m
-    
+
     m = MinHash.minhash32(["bar", "foo", "baz"], 23)
     assert_equal 568256043, m
-    
+
     m = MinHash.minhash32(["baz", "bar", "foo"], 23)
     assert_equal 568256043, m
   end
